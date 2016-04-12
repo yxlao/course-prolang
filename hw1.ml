d609e548c2ded6f127fb8a23a3a9d8d7b8cec76f
Yixing Lao <linuxthink@gmail.com>
2016-04-11 21:38:31 -0700

    digitsOfInt

--- hw1.ml
+++ hw1.ml
@@ -22,6 +22,10 @@ let rec sumList xs = match xs with
 
 (* digitsOfInt : int -> int list  *)
 let rec digitsOfInt n = failwith "TBD:digitsOfInt"
+let rec digitsOfInt n = 
+  if n < 0 then []
+  else if n < 10 then n
+  else digitsOfInt (n / 10) @ [n mod 10]
 
 
 (* uncomment and run AFTER you have implemented digitsOfInt 
