0fb2d96ca67a808831261abc99c5545cfa83e654
Yixing Lao <linuxthink@gmail.com>
2016-04-11 21:28:10 -0700

    sumList

--- hw1.ml
+++ hw1.ml
@@ -3,7 +3,10 @@
 *)
 
 (* sumList : int list -> int *) 
-let rec sumList xs = failwith "TBD:sumList"
+let rec sumList xs = match xs with
+  | []    -> 0
+  | [a]   -> a
+  | (h::t) -> h + sumList t
 
 (* uncomment and run AFTER you have implemented sumList
 
