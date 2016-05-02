171a3c83049426d4671c5936f0547f5c465f66e8
Yixing Lao <linuxthink@gmail.com>
2016-05-02 04:21:36 -0700

    mulByDigit works, 23/25

--- hw3.ml
+++ hw3.ml
@@ -180,6 +180,10 @@ let bigAdd l1 l2 =
 
 let rec mulByDigit i l = failwith "to be implemented"
 
+let rec mulByDigit i l = match i with
+  | 0 -> []
+  | _ -> bigAdd l (mulByDigit (i-1) l)
+
 (* UNCOMMENT AFTER IMPLEMENTING THE ABOVE
 
    let _ = mulByDigit 9 [9;9;9;9]
