9554a133277280e7f878fd7d8511b8454101bc8c
Yixing Lao <linuxthink@gmail.com>
2016-06-06 21:24:49 -0700

    2(a) tracing

--- hw6/Decorators.scala
+++ hw6/Decorators.scala
@@ -20,12 +20,28 @@ object Decorators {
   }
 
   object trace {
+    // You may add more fields here
+    private var level = 0
 
-      // You may add more fields here
     def apply[A, B](name: String)(f: A => B) : Function1[A, B] = new Function1[A, B] {
       // You may add more fields here
 
-      def apply (x: A): B = sys.error("TO BE DONE")
+      def apply (x: A): B = {
+        // print topStr
+        var topStr = "| " * level + ",- "  + name + "(" + x.toString + ")"
+        println(topStr)
+        // increment level
+        level += 1
+        // func call
+        val result = f(x)
+        // decrement level
+        level -= 1
+        // print bottomStr
+        var bottomStr = "| " * level + "`- " + result.toString
+        println(bottomStr)
+        // return
+        result
+      }
     }
   }
 
